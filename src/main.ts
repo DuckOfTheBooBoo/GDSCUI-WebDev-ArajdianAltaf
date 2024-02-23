1ffe46a824b0e18dad5a7a5119f6bd1d03f5e61b
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-02-23 16:49:48 +0700

    Environment Initialization

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'animate.css'
+import PrimeVue from 'primevue/config'
+
+
+const app = createApp(App)
+app.mount('#app')
+app.use(PrimeVue)
