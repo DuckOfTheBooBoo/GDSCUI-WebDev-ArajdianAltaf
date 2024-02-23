9b37ce853901888785ddd5e9313ae2644f96bf8d
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-02-23 14:31:58 +0000

    App.vue init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import TaskGroup from './components/TaskGroup.vue'
 </script>
 
 <template>
-  
+    <div class="m-5">
+        <h1>Tasks</h1>
+        <TaskGroup />
+        <h1>Completed Task</h1>
+    </div>
 </template>
 
 <style scoped>
