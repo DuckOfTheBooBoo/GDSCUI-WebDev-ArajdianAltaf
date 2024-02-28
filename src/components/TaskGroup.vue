daa27edb41f9b3842cf6c5ed9368b8c22b62936b
Arajdian Altaf <arajdianaltaf123@gmai.com>
2024-02-28 14:24:36 +0700

    feat: show message if no task exist

--- src/components/TaskGroup.vue
+++ src/components/TaskGroup.vue
@@ -1,34 +1,39 @@
 <script setup lang="ts">
 import TaskItem from './TaskItem.vue'
-import { ref, onMounted, onBeforeMount } from 'vue';
+import { ref, onBeforeMount, onMounted, computed } from 'vue';
 import Task from '../models/Task'
 import { useTodoStore } from '../stores/todoStores';
 
 const tasks = ref<Task[]>([])
 const todoStore = useTodoStore()
 
+// const forceUpdate = ref(0)
+
 const fetchTasks = () => {
     console.log('Fetching task...')
-    tasks.value = todoStore.getAllTasks()
+    const newTasks = todoStore.getAllTasks()
+    console.log(newTasks)
+    tasks.value = newTasks
+    // forceUpdate.value++
 }
+
 
 onMounted(() => {
     fetchTasks()
-    todoStore.eventEmitter.on('taskUpdated', fetchTasks)
+    todoStore.eventEmitter.on('tasksUpdated', fetchTasks)
 })
 </script>
 
 <template>
-    <div class="mx-3 flex flex-col gap-2">
+    <div v-if="tasks.length > 0" class="mx-3 flex flex-col gap-2">
         <Button @click="fetchTasks">Fetch Task</Button>
         <TaskItem v-for="task in tasks"
             :task-id="task.id"
-            :title="task.title"
-            :due-date="task.dueDate"
-            :priority="task.priority"
-            :group="task.group"
-            :is-completed-prop="task.completed"
+            :key="task.id"
         />
+    </div>
+    <div v-else class="flex justify-center items-center">
+        <p class="text-gray-400 my-10">Such empty... Create a new task would ya?</p>
     </div>
 </template>
 
