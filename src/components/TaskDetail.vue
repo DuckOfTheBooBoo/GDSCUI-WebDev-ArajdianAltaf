becf7714a36cb3f883c7e4883654791f88dfce71
Arajdian Altaf <arajdianaltaf123@gmai.com>
2024-02-28 14:25:23 +0700

    feat: populate task details

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,50 +1,57 @@
 <script setup lang="ts">
 import Tag from 'primevue/tag';
+import { useTodoStore } from '../stores/todoStores';
+import { reactive, Ref, ref } from 'vue';
+import Task from '../models/Task';
+import Group from '../models/Group';
 
-defineProps({
-    title: String,
-    subTask: Array,
-    dueDate: Date,
-    groupType: String,
-    priority: Number
-})
+const props = defineProps<{
+    taskId: number
+}>()
+
+const todoStore = useTodoStore()
+
+let task: Task = reactive({} as Task)
+if (task) {
+    task = todoStore.getTask(props.taskId)!
+} else {
+    console.log('Task is undefined', task)
+}
+
+const taskGroup: Ref<Group> = ref({} as Group)
+taskGroup.value =  todoStore.getGroup(task.group)!
+
+if (!taskGroup.value) {
+    console.log('taskGroup is undefined', taskGroup.value)
+}
+
 </script>
 
 <template>
     <p class="font-bold text-3xl mb-2">Task Detail</p>
     <div class="flex gap-1 mb-5">
         <!-- Group -->
-        <Tag value="General" rounded>
-            <span class="text-xs">General</span>
+        <Tag :style="`background-color: ${taskGroup?.color};`" rounded>
+            <span class="text-xs">{{ taskGroup?.name }}</span>
         </Tag>
 
         <!-- Priority -->
-        <Tag v-if="priority === 1" severity="info" rounded>
+        <Tag v-if="task.priority === 1" severity="info" rounded>
             <span class="text-xs">Low</span>
         </Tag>
-        <Tag v-else-if="priority === 2" value="Medium" severity="warning" rounded>
+        <Tag v-else-if="task.priority === 2" value="Medium" severity="warning" rounded>
             <span class="text-xs">Medium</span>
         </Tag>
-        <Tag v-else-if="priority === 3" value="High" severity="danger" rounded>
+        <Tag v-else-if="task.priority === 3" value="High" severity="danger" rounded>
             <span class="text-xs">High</span>
         </Tag>
 
         <!-- Due Date -->
         <Tag icon="pi pi-calendar" class="!bg-orange-600" rounded>
-            <span class="text-xs">09/08/2005</span>
+            <span class="text-xs">{{ task?.dueDate.toLocaleDateString('en-GB') }}</span>
         </Tag>
     </div>
     <p class="font-bold text-xl">Description</p>
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex commodi necessitatibus repellat quia blanditiis
-        omnis excepturi, architecto aliquid earum tenetur nemo reiciendis eum nostrum amet tempora fugit quam atque
-        totam.
-
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta vitae voluptas officia autem maxime, fugit
-        cupiditate tenetur amet possimus maiores atque corrupti perspiciatis dolores beatae tempora excepturi
-        eveniet, quis necessitatibus?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, iusto vero amet sit deserunt animi.
-        Laudantium neque amet, error nihil quae possimus, ad, placeat aliquid beatae cum officia distinctio aliquam.
-
-    </p>
+    <p v-if="task?.description">{{ task?.description }}</p>
+    <p v-else class="text-gray-400">No description provided</p>
 </template>
